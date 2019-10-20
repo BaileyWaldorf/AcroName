31335ea7bd110972e0a59c2172cd7c4aa96f043d
Dax <Daxtubach@gmail.com>
2019-10-20 02:50:54 -0400

    git

---
+++ client/build/precache-manifest.bcd2fa2c9f96c24515a2c16baeebd81e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "714a0c48cd844474a88b634b34df8e1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c25c87934119c5a1d2d1",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "84733ee794e8bf907c53",
+    "url": "/static/css/main.f1430c33.chunk.css"
+  },
+  {
+    "revision": "c25c87934119c5a1d2d1",
+    "url": "/static/js/2.d98f91e8.chunk.js"
+  },
+  {
+    "revision": "84733ee794e8bf907c53",
+    "url": "/static/js/main.5f87cfa1.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
